4d296d213727b3b6d5396b6ddb1aec8409d6595c
Coml <Coml@users.noreply.github.com>
2017-05-30 00:25:09 +0100

    Inital

---
+++ bash/customisations.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+sudo rm /etc/update-motd.d/10-help-text
+sudo rm /etc/update-motd.d/00-header
+
+sudo cp ./00-logo /etc/update-motd.d/00-logo
+sudo cp ./01-distro /etc/update-motd.d/01-distro
+
+
+cat <<EOF >> ~/.bashrc
+export PS1="[\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;243m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]] [\[$(tput sgr0)\]\[\033[38;5;196m\]\w\]\[\033[38;5;15m\]] \\$\[$(tput sgr0)\]"
+EOF
