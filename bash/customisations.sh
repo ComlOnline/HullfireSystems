a534c71aa9a6d4a54f0e7040e7129176447ca2bc
Coml <Coml@users.noreply.github.com>
2017-05-30 00:48:23 +0100

    add chmod +x

--- bash/customisations.sh
+++ bash/customisations.sh
@@ -7,6 +7,10 @@ sudo cp ./00-logo /etc/update-motd.d/00-logo
 sudo cp ./01-distro /etc/update-motd.d/01-distro
 
 
+sudo chmod +x /etc/update-motd.d/00-logo
+sudo chmod +x /etc/update-motd.d/01-distro
+
+
 cat <<EOF >> ~/.bashrc
 export PS1="[\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;243m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]] [\[$(tput sgr0)\]\[\033[38;5;196m\]\w\]\[\033[38;5;15m\]] \\$\[$(tput sgr0)\]"
 EOF
