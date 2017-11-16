ef8a5b3a07d7841078f1fd1e6bba5ae77048d477
ComlOnline <ComlOnline@users.noreply.github.com>
2017-11-16 09:40:36 +0000

    Bug fix
    THe export line was filled with rubbish causing unwanted behaviour at the shell prompt

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HullfireSystems
+Customizing Bash Prompt for Hullfire Machines
+
+Just run:
+`curl -sSL https://raw.githubusercontent.com/ComlOnline/HullfireSystems/master/bash/customisations.sh | bash`
+
+Warning some of the info will be incorrect so take it with a pinch of salt
--- bash/customisations.sh
+++ bash/customisations.sh
@@ -1,23 +1,70 @@
 #! /bin/bash
 
-
+#Update the Repositories
 sudo apt-get update
+
+#Install figlet
 sudo apt-get install -y figlet
 
+#Remove previous versions and other motd things
 sudo rm /etc/update-motd.d/10-help-text
 sudo rm /etc/update-motd.d/00-header
 sudo rm /etc/update-motd.d/00-logo
 sudo rm /etc/update-motd.d/01-distro
 sudo rm /etc/update-motd.d/01-info
 
-sudo cp ./00-logo /etc/update-motd.d/00-logo
-sudo cp ./01-info /etc/update-motd.d/01-info
+#Script to make the logo
+cat <<EOF00 >> /etc/update-motd.d/00-logo
+#! /bin/bash
 
+figlet "Hullfire Systems"
+EOF00
 
+#Script to display system info
+cat <<EOF01 >> /etc/update-motd.d/01-info
+#!/bin/sh
+
+UPTIME_DAYS=$(expr `cat /proc/uptime | cut -d '.' -f1` % 31556926 / 86400)
+UPTIME_HOURS=$(expr `cat /proc/uptime | cut -d '.' -f1` % 31556926 % 86400 / 3600)
+UPTIME_MINUTES=$(expr `cat /proc/uptime | cut -d '.' -f1` % 31556926 % 86400 % 3600 / 60)
+
+cat << EOF
+%                                                                            %
+%+++++++++++++++++++++++++++++++ SERVER INFO ++++++++++++++++++++++++++++++++%
+%                                                                            %
+	Name: `hostname`
+	Uptime: $UPTIME_DAYS days, $UPTIME_HOURS hours, $UPTIME_MINUTES minutes
+
+	CPU: `cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d':' -f2`
+	Memory: `free -m | head -n 2 | tail -n 1 | awk {'print $2'}`M
+	Swap: `free -m | tail -n 1 | awk {'print $2'}`M
+	Disk: `df -h / | awk '{ a = $2 } END { print a }'`
+	Distro: `lsb_release -s -d` with `uname -r`
+
+	CPU Load: `cat /proc/loadavg | awk '{print $1 ", " $2 ", " $3}'`
+	Free Memory: `free -m | head -n 2 | tail -n 1 | awk {'print $4'}`M
+	Free Swap: `free -m | tail -n 1 | awk {'print $4'}`M
+	Free Disk: `df -h / | awk '{ a = $2 } END { print a }'`
+
+	External Address: `ifconfig ens160 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`
+	Internal Address: `ifconfig ens192 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`
+EOF
+EOF01
+
+#Make the above executable
 sudo chmod +x /etc/update-motd.d/00-logo
 sudo chmod +x /etc/update-motd.d/01-info
 
+#Clean out other shell customisations
+sed -i.bak '/export PS1/d' $HOME/.bashrc
 
-cat <<EOF >> ~/.bashrc
-export PS1="[\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;243m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]] [\[$(tput sgr0)\]\[\033[38;5;196m\]\w\]\[\033[38;5;15m\]] \\$\[$(tput sgr0)\]"
-EOF
+#Colour for bash
+cat <<EOFBASHRC >> ~/.bashrc
+
+MYRED="\[\033[38;5;196m\]"
+MYGREY="\[\033[38;5;243m\]"
+MYWHITE="\[\033[38;5;15m\]"
+
+export PS1="$MYWHITE[$MYRED\h$MYGREY \u$MYWHITE] [$MYRED\w$MYWHITE] "
+
+EOFBASHRC
